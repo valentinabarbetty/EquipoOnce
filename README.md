cb3304df2a563892a601e1aef5374ff90d45db01
valentinabarbetty <valentinabarbetty2@gmail.com>
2024-05-05 12:16:12 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# EquipoOnce
+Repositorio para el avance del proyecto #1 de desarrollo de aplicaciones móviles del equipo 11.
---
+++ app/src/main/java/com/uv/dogappuv/view/fragments/LoginFragment.kt
@@ -0,0 +1,38 @@
+package com.uv.dogappuv.view.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.uv.dogappuv.databinding.FragmentLoginBinding
+import com.uv.dogappuv.R
+import java.util.Calendar
+
+
+class LoginFragment : Fragment() {
+
+
+    private lateinit var binding: FragmentLoginBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentLoginBinding.inflate(inflater)
+        binding.lifecycleOwner = this
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        navigationFragmentAdmin()
+    }
+    private fun navigationFragmentAdmin(){
+        binding.btnFragmentAdmin.setOnClickListener {
+            findNavController().navigate(R.id.fragment_admin_citas)
+        }
+    }
+}
