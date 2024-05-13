2e057ebccba4b129492e17740917d68dde096738
Eyder Santiago Suarez Chavez <eyder.suarez@correounivalle.edu.co>
2024-05-13 11:45:51 -0500

    imagen dinámica OK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,7 +48,9 @@ dependencies {
     implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
     implementation("androidx.room:room-common:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
+    implementation("com.airbnb.android:lottie:6.4.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
 }
